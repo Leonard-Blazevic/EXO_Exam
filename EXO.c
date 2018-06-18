67674fbd9afd65f6966f0163dbf2325783cb1f1d
leonard_blazevic <leonard.blazevic@fer.hr>
2018-06-18 14:10:45 +0200

    Changed the prescaler assumption and the mlmc period.

--- EXO.c
+++ EXO.c
@@ -1,7 +1,7 @@
 /*since the desired periods are expressed with their max values, I created a sequence with periods:
 1) cfrs => 200us
 2) hsfods => 400us
-3) mlmc => 400us
+3) mlmc => 800us
 */
 
 void cfrs (void); // First task prototype (max period 200us)
@@ -12,12 +12,14 @@ void Timer_Init (void) // The interrupt is generated every 100us
 {
     Stop_Timer();
     CNTREG = 0x0; // The timer starts from 0
-    CMPREG = 0x1; // The interrupt will be generated every tick coming from the prescaler
-    PRES = 0xFFFFFBC0; // 2^32 - 1088 => scales the frequency visible to the timer down to 10 kHz
+    CMPREG = 0x11; // 17(DEC) => The interrupt will be generated every 17. tick coming from the prescaler => output frequency of 10kHz
+    PRES = 0x00000006; // 110 => address input of the multiplexer which sends the output of the 6. D bistable through,
+                      // which gives a frequency fout = fin/2^6 = 170 kHz (10880 / 64 = 170)
     Start_Timer();
 }
 
 uint32_t Time = 0; // Global variable for task scheduling
+int run_mlmc = 0; // Flag that is used for skipping the execution of the mlmc task in the middle of the cycle
 
 
 void Timer_ISR () // Interrupt routine
@@ -27,8 +29,9 @@ void Timer_ISR () // Interrupt routine
     Task_Dispatcher();
 }
                         //                              <-------400us------->
-void Task_Dispatcher () // Task sequence => cfrs, hsfods, cfrs, mlmc, cfrs, hsfods, cfrs, mlmc
-                       //                       <--200us-->          <---------400us------>
+void Task_Dispatcher () // Task sequence => cfrs, hsfods, cfrs, EMPTY, cfrs, hsfods, cfrs, mlmc
+                       //                       <--200us-->
+                       //                   <-------------------------800us-------------------->
 {
     Timer_Init(); // Timer reinitialization
 
@@ -41,7 +44,10 @@ void Task_Dispatcher () // Task sequence => cfrs, hsfods, cfrs, mlmc, cfrs, hsfo
             hsfods(); // Second task
             break;
         case 4:
-            mlmc(); // Third task
+            if (run_mlmc == 1) // Execute?
+                mlmc(); // Third task
+            
+            run_mlmc = run_mlmc == 0 ? 1 : 0; // Invert the value of the flag
             Time = 0; // Starting the sequence again
             break;
     }  
